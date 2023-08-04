6d8cf9e34224ba95cbf1fefca12072b552c65cd4
Rodrigo Lima <rodrigo@capyba.com>
2023-08-04 16:21:04 -0300

    Add user admin

--- apps/user/admin.py
+++ apps/user/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from .models import User
+
+
+class UserAdmin(DjangoUserAdmin):
+    list_display = ('email', 'name', 'is_staff')
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal info'), {'fields': ('name', 'avatar')}),
+        (_('Permissions'), {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        (_('Important dates'), {'fields': ('last_login',)}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'name', 'password1', 'password2'),
+        }),
+    )
+    readonly_fields = ['last_login',]
+    search_fields = ('name', 'email')
+    ordering = ['email']
+
+
+admin.site.register(User, UserAdmin)
